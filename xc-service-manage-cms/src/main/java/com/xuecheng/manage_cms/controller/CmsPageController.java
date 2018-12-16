dd4f5eaaf3478fa400f4fa68eaed17fa13f4687c
Lifewang <lifewangbl@foxmail.com>
2018-12-16 17:10:14 +0800

    修改controller测试类的测试方法

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
@@ -9,6 +9,7 @@ import com.xuecheng.manage_cms.framework.model.response.QueryResponseResult;
 import com.xuecheng.manage_cms.framework.model.response.QueryResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,20 +22,19 @@ import java.util.List;
  */
 
 @RestController
+@RequestMapping("/cms/page")
 public class CmsPageController implements CmsPageControllerApi {
 
     @GetMapping("/list/{page}/{size}")
     public QueryResponseResult findList(@PathVariable("page") int page, @PathVariable("size") int
             size, QueryPageRequest queryPageRequest) {
-        //暂时采用测试数据，测试接口是否可以正常运行
-        QueryResult queryResult = new QueryResult();
-        queryResult.setTotal(2L);
-        //静态数据列表
-        List list = new ArrayList();
+        QueryResult<CmsPage> queryResult = new QueryResult<>();
+        List<CmsPage> list = new ArrayList<>();
         CmsPage cmsPage = new CmsPage();
         cmsPage.setPageName("测试页面");
         list.add(cmsPage);
         queryResult.setList(list);
+        queryResult.setTotal(1);
         QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
         return queryResponseResult;
     }
